44571be886331cbc3f63202a5f41e65d6e61bc43
Vaakoo29 <37950549+Vaakoo29@users.noreply.github.com>
2021-04-07 18:28:27 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import nxviz as nv 
+import json
+# from nxviz import annotate
+
+with open('investments.json', 'r') as file:
+    data = json.load(file)
+
+G = nx.Graph() # создание графа
+
+for project in data['projects']:
+    G.add_node(project['id'], label=project['name'], weight=project['amount'], region=project['area'])
+    print(G.nodes()) #почему-то сбивается, но результат выходит нормальный! 
+    if len(project['links']) > 0:
+        for link in project['links']:
+            G.add_edge(project['id'], link)
+
+print(list(G.nodes(data=True)))
+# print(list(G.edges())) 
+
+nx.draw(G, with_labels=True)
+# nx.draw(G)
+plt.show()
+
+
+# Из игры престолов визуализация, только надо еще pip install scipy
+# pos = nx.kamada_kawai_layout(G)
+# nx.draw_networkx_nodes(G, pos, node_color='yellow', node_size=10)
+# nx.draw_networkx_edges(G, pos, edge_color='green', width=0.2)
+# nx.draw_networkx_labels(G, pos, font_size=7, font_family='Arial')
+# plt.axis('off')
+# plt.show()
